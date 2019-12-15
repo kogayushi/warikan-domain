3a2a0e739b9ef6cb71a88a2df28104447c3f4ab9
yushi.koga <youshe.old.pleasure@gmail.com>
2019-12-15 22:11:41 +0900

    implement 請求金額 class

--- src/main/java/warikan/domain/model/請求金額.java
+++ src/main/java/warikan/domain/model/請求金額.java
@@ -1,3 +1,16 @@
 package warikan.domain.model;
 
-public class 請求金額 {}
+public class 請求金額 {
+    private final int value;
+
+    public 請求金額(int value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("請求金額は1円以上を設定してください。");
+        }
+        this.value = value;
+    }
+
+    public int 値を求める() {
+        return this.value;
+    }
+}
