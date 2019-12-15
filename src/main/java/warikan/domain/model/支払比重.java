e2bd34cd168d2c29712dd4e7140e641e60f0df4f
yushi.koga <youshe.old.pleasure@gmail.com>
2019-12-15 22:10:31 +0900

    implement 支払比重 class

--- src/main/java/warikan/domain/model/支払比重.java
+++ src/main/java/warikan/domain/model/支払比重.java
@@ -1,3 +1,16 @@
 package warikan.domain.model;
 
-public class 支払比重 {}
+public class 支払比重 {
+    private final int _値;
+
+    public 支払比重(int _値) {
+        if (_値 <= 0) {
+            throw new IllegalArgumentException("比重の値は必ず1以上である必要があります");
+        }
+        this._値 = _値;
+    }
+
+    public int 値を求める() {
+        return this._値;
+    }
+}
